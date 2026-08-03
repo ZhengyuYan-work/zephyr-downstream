e43e67c25fa7924c1467d269315f46807cf5f5ec
Zhengyu Yan <my_computer@LAPTOP-H5AIQJ6Q.localdomain>
2026-08-03 19:59:52 +0800

    drivers: reset: refactor SF32LB reset driver to use ll_rcc.h

--- drivers/reset/reset_sf32lb.c
+++ drivers/reset/reset_sf32lb.c
@@ -10,17 +10,21 @@
 #include <zephyr/devicetree.h>
 #include <zephyr/drivers/reset.h>
 
-#define SF32LB_RESET_OFFSET(id) ((((id) >> 5U) & 0x1U) << 2U)
+#include <ll_rcc.h>
+
+#define SF32LB_RESET_REG(id) (((id) >> 5U) & 0x1U)
+#define SF32LB_RESET_BIT(id) ((id) & 0x1FU)
 
 struct sf32lb_reset_config {
-	uintptr_t base;
+	HPSYS_RCC_TypeDef *rcc;
 };
 
 static int sf32lb_reset_status(const struct device *dev, uint32_t id, uint8_t *status)
 {
 	const struct sf32lb_reset_config *config = dev->config;
+	uint32_t mask = BIT(SF32LB_RESET_BIT(id));
 
-	*status = !!sys_test_bit(config->base + SF32LB_RESET_OFFSET(id), id);
+	*status = !!ll_rcc_is_module_in_reset(config->rcc, SF32LB_RESET_REG(id), mask);
 
 	return 0;
 }
@@ -28,8 +32,9 @@ static int sf32lb_reset_status(const struct device *dev, uint32_t id, uint8_t *s
 static int sf32lb_reset_line_assert(const struct device *dev, uint32_t id)
 {
 	const struct sf32lb_reset_config *config = dev->config;
+	uint32_t mask = BIT(SF32LB_RESET_BIT(id));
 
-	sys_set_bit(config->base + SF32LB_RESET_OFFSET(id), id);
+	ll_rcc_reset_module(config->rcc, SF32LB_RESET_REG(id), mask);
 
 	return 0;
 }
@@ -37,8 +42,9 @@ static int sf32lb_reset_line_assert(const struct device *dev, uint32_t id)
 static int sf32lb_reset_line_deassert(const struct device *dev, uint32_t id)
 {
 	const struct sf32lb_reset_config *config = dev->config;
+	uint32_t mask = BIT(SF32LB_RESET_BIT(id));
 
-	sys_clear_bit(config->base + SF32LB_RESET_OFFSET(id), id);
+	ll_rcc_release_reset(config->rcc, SF32LB_RESET_REG(id), mask);
 
 	return 0;
 }
@@ -59,7 +65,7 @@ static DEVICE_API(reset, sf32lb_reset_api) = {
 };
 
 static const struct sf32lb_reset_config sf32lb_reset_cfg = {
-	.base = DT_REG_ADDR(DT_INST_PARENT(0)),
+	.rcc = (HPSYS_RCC_TypeDef *)DT_REG_ADDR(DT_INST_PARENT(0)),
 };
 
 DEVICE_DT_INST_DEFINE(0, NULL, NULL, NULL, &sf32lb_reset_cfg, PRE_KERNEL_1,
